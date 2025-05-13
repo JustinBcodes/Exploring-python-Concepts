550aebb28483eaa8cb706d659aea8906288d3d1a
Justin Bean <justin@Justins-MacBook-Air-2.local>
2025-05-12 20:10:30 -0500

    Add code for arithmetic operations and number check

---
+++ exploring_python_conceptss.py
@@ -0,0 +1,29 @@
+##Task 1
+name = 'Justin'
+age = int(19)
+height = float(6.5)
+
+print(f"Name: {name}, Age: {age}, Height: {height}")
+
+##Task 2
+
+num1 = int(10)
+num2 = int(20)
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 /num2
+
+print("Addition:", add, "Subtraction:", sub, "Multiplication:", mul, "Division:", div)
+
+##Task 3
+num = (input("Please enter a number:"))
+input("Please enter a number:")
+
+if num > 0:
+    print("This number is positive awesome")
+elif num < 0:
+    print("This number is negative. Better luck next time")
+else:
+    print("Zero it is a perfect balance.")
